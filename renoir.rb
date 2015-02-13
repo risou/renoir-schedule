cd406efda1aaeb08bc012192a67a0f2e85f100e2
Masaaki FUJISAWA <m.fujisawa@coosy.co.jp>
2015-02-13 13:08:45 +0900

    必要な情報だけ取得する仮実装

--- renoir.rb
+++ renoir.rb
@@ -11,12 +11,14 @@ date_key = 'date'
 agent = Mechanize.new
 
 shops["shops"].each do |key, value|
-	p key
-	p value
 
 	today = Date::today
-	for num in 0..0
-	# for num in 0..59
+
+	# for num in 0..0
+	for num in 0..59 do
+    if ![0, 6].include?((today + num).wday)
+      next
+    end
 		day = (today + num).to_s
 
 		sleep(1)
@@ -26,14 +28,16 @@ shops["shops"].each do |key, value|
 		rooms.each do |room|
 			tds = room.xpath('td')
 			room_info = tds[0].inner_text
-			for i in 1..tds.size-1
+      usable = []
+			for i in 1..tds.size-1 do
 				if tds[i][:title].nil?
-					p tds[i][:bgcolor]
 				else
-					p tds[i][:title]
+					usable.push i
 				end
 			end
-			break
+      if usable & [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22] == [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+        p room_info + ' ' + day
+      end
 		end
 	end
 end
